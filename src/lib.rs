ecde0675664aa38416ee2a14c0f379bf51b211f7
Wenzel-Z <76844267+Wenzel-Z@users.noreply.github.com>
2023-07-07 16:40:35 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+use std::thread::JoinHandle;
+use std::{fmt, thread};
+use std::sync::{Arc, mpsc, Mutex};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+#[derive(Debug, Clone)]
+pub struct PoolCreationError;
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid number for size of pool")
+    }
+}
+
+#[derive(Debug)]
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move|| loop {
+            let message = receiver.lock().unwrap().recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+impl ThreadPool {
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size < 1 {
+            return Err(PoolCreationError);
+        }
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        Ok(ThreadPool {
+            workers,
+            sender: (Some(sender)),
+        })
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+use WebServer::{ThreadPool};
+
+fn main() {
+    const HOST: &str = "127.0.0.1:7878";
+    let listener = TcpListener::bind(HOST).unwrap();
+    let pool = ThreadPool::build(4).unwrap();
+
+    dbg!("{}", &pool);
+    dbg!(HOST);
+    // Iterator over connections received
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap(); // unwrap the stream to get the TCP stream
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    // read in the first line from the stream using .next on a BufReader
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    // assign status_line and filename
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    // Empty body response
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
